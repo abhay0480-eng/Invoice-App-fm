bad8dd91909d374190aed226ad27a2bbb5962458
abhay0480sharma <abhay@openinnovationslab.com>
2024-01-13 18:05:20 +0530

    theme applied on dashboard detail  desktop

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -53,12 +53,12 @@ const DetailPage = () => {
       </Backdrop>
     <ConfirmDelete   handleCloseDelete={handleCloseDelete} openDelete={openDelete} detailsid = {details.$id} />
     <FormModal handleOpen={handleOpen} handleClose={handleClose} open={open} details={details} addNew={false}/>
-    <div className='max-w-5xl mx-auto p-5 lg:mt-10'>
+    <div className='max-w-5xl mx-auto p-5 bg-[#141625] lg:mt-10'>
     <Link to='/' className={`${theme?"text-[#fff]":"text-[#0C0E16]"}  text-[15px] font-bold mb-5`}>Go back</Link>
     <div className={`grid grid-cols-2  lg:grid-cols-7 content-center gap-x-5 ${!theme?"bg-white":"bg-[#1E2139]"} p-5 rounded-lg mt-3`}>
-      <p className='text-[#858BB2] text-[13px] font-medium my-auto'>Status</p>
+      <p className={` ${theme?"text-[#DFE3FA]":"text-[#858BB2]"}  text-[13px] font-medium my-auto`}>Status</p>
       <div className='lg:col-span-3'>
-      <button className={`p-3 rounded-lg col-span-3  flex justify-center items-center w-[150px] mx-2  font-bold ${status==="paid"?"bg-[#33D69F] text-[#33D69F] bg-opacity-[0.0571]":status==="pending"?"bg-[#FF8F00] text-[#FF8F00] bg-opacity-[0.0571]":"bg-[#373B53] text-[#373B53] bg-opacity-[0.0571]"}`}>
+      <button className={`p-3 rounded-lg col-span-3  flex justify-center items-center  w-[118px] lg:w-[150px] mx-2  font-bold ${status==="paid"?"bg-[#33D69F] text-[#33D69F] bg-opacity-[0.0571]":status==="pending"?"bg-[#FF8F00] text-[#FF8F00] bg-opacity-[0.0571]":"bg-[#373B53] text-[#373B53] bg-opacity-[0.0571]"}`}>
         <div className={`w-2 h-2 rounded-full mx-2 ${status==="paid"?"bg-[#33D69F]":status==="pending"?"bg-[#FF8F00] ":"bg-[#373B53] "}`} ></div>{status.charAt(0).toUpperCase() + status.slice(1)}
       </button>
       </div>
@@ -70,14 +70,12 @@ const DetailPage = () => {
     <div className={`${!theme?"bg-white":"bg-[#1E2139]"} p-5 lg:p-10 mt-10 h-[100vh] lg:h-auto`}>
       <div className='lg:flex justify-between '>
         <div>
-        {/* <p className='text-[#0C0E16] text-[15px] font-bold  m-auto'><span className='text-[#7E88C3] '>#</span>{details.$id}</p> */}
         <p className={`${theme?"text-[#fff]":"text-[#0C0E16]"} text-[15px] font-bold  lg:m-auto`}><span className='text-[#7E88C3] '>#</span>{details.$id}</p>
 
-        <p className='text-[13px] font-medium text-[#7E88C3] my-1'>{details.description}</p>
+        <p className={`text-[13px] ${theme?"text-[#DFE3FA]":"text-[#7E88C3]"} font-medium  my-1`}>{details.description}</p>
         </div>
 
-        <div className=' my-3 lg:my-auto lg:text-right text-[#7E88C3] text-[13px] font-medium'>
-          <p>{details.senderAddressStreet}</p>
+        <div className= {`my-3 lg:my-auto lg:text-right ${theme?"text-[#DFE3FA]":"text-[#7E88C3]"} text-[13px] font-medium `}>
           <p>{details.senderAddressCity}</p>
           <p>{details.senderAddressPostCode}</p>
           <p>{details.senderAddressCountry}</p>
@@ -86,25 +84,25 @@ const DetailPage = () => {
 
       <div className='grid lg:grid-cols-4 grid-cols-2 mt-3'>
       <div>
-        <p className='text-[13px] font-medium text-[#7E88C3] my-1'>Invoice Date</p>
-        <p className='text-[#0C0E16] text-[15px] font-bold  m-auto'>{dateOnly}</p>
+        <p className={`text-[13px] font-medium  ${theme?"text-[#DFE3FA]":"text-[#7E88C3]"} my-2`}>Invoice Date</p>
+        <p className={`${theme?"text-[#DFE3FA]":"text-[#0C0E16]"} text-[15px] font-bold  m-auto`}>{dateOnly}</p>
       </div>
         <div>
-        <p className='text-[13px] font-medium text-[#7E88C3] my-1'>Bill To</p>
-        <p className='text-[#0C0E16] text-[15px] font-bold  m-auto'>{details.clientName}</p>
+        <p className={`text-[13px] font-medium  ${theme?"text-[#DFE3FA]":"text-[#7E88C3]"} my-2`}>Bill To</p>
+        <p className={`${theme?"text-[#DFE3FA]":"text-[#0C0E16]"} text-[15px] font-bold  m-auto`}>{details.clientName}</p>
         </div>
         <div className='hidden lg:block'>
-        <p className='text-[13px] font-medium text-[#7E88C3] my-1'>Sent to</p>
-        <p className='text-[#0C0E16] text-[15px] font-bold  m-auto'>{details.clientEmail}</p>
+        <p className={`text-[13px] font-medium  ${theme?"text-[#DFE3FA]":"text-[#7E88C3]"} my-2`}>Sent to</p>
+        <p className={`${theme?"text-[#DFE3FA]":"text-[#0C0E16]"} text-[15px] font-bold  m-auto`}>{details.clientEmail}</p>
         </div>
       </div>
 
       <div className='grid  lg:grid-cols-4 grid-cols-2 mt-3'>
         <div>
-          <p className='text-[13px] font-medium text-[#7E88C3] my-1'>Payment Due</p>
-          <p className='text-[#0C0E16] text-[15px] font-bold  m-auto'>{details.paymentDue}</p>
+          <p className={`text-[13px] font-medium  ${theme?"text-[#DFE3FA]":"text-[#7E88C3]"} my-2`}>Payment Due</p>
+          <p className={`${theme?"text-[#DFE3FA]":"text-[#0C0E16]"} text-[15px] font-bold  m-auto`}>{details.paymentDue}</p>
         </div>
-        <div className='text-left lg:col-span-3 text-[#7E88C3] text-[13px] font-medium'>
+        <div className={`text-left lg:col-span-3 ${theme?"text-[#DFE3FA]":"text-[#7E88C3]"} text-[13px] font-medium`}>
           <p>{details.clientAddressStreet}</p>
           <p>{details.clientAddressCity}</p>
           {/* <p>{details.clientAddress.postCode}</p> */}
@@ -117,7 +115,7 @@ const DetailPage = () => {
         <p className='text-[#0C0E16] text-[15px] font-bold  m-auto'>{details.clientEmail}</p>
         </div>
 
-      <div className='bg-[#F9FAFE] w-full mt-2 rounded-b-2xl'>
+      <div className={`${theme?"bg-[#252945]":"bg-[#F9FAFE]"} w-full mt-10 lg:mt-2 rounded-lg`}>
         <div className='lg:grid lg:grid-cols-5 hidden text-[#7E88C3] text-[13px] font-medium  pt-5 lg:pt-10 px-10 mt-5 rounded-lg'>
           <p className='col-span-2'>Item Name</p>
           <p className='text-right'>QTY.</p>
@@ -125,15 +123,15 @@ const DetailPage = () => {
           <p className='text-right'>Total</p>
         </div>
 
-        <div className='py-5'>
+        <div className='py-5 rounded-lg'>
         {details.items.map((item,index)=>{
           return(
           <div key={index}>
           <div  className='lg:grid lg:grid-cols-5 text-[#7E88C3] hidden  text-[13px] font-medium px-5 lg:px-10 py-2 rounded-lg'>
-            <p className='col-span-2 text-[#0C0E16] text-[15px] font-bold'>{item.name}</p>
-            <p className=' text-[#7E88C3] text-[15px] font-bold text-right '>{item.quantity}</p>
-            <p className=' text-[#7E88C3] text-[15px] font-bold text-right '>£{item.price}</p>
-            <p className=' text-[#0C0E16] text-[15px] font-bold text-right'>£{item.total}</p>
+            <p className= {`col-span-2 ${theme?"text-[#fff]":"text-[#0C0E16]"}  text-[15px] font-bold`}>{item.name}</p>
+            <p className= {` ${theme?"text-[#fff]":"text-[#7E88C3]"}  text-[15px] font-bold text-right`} >{item.quantity}</p>
+            <p className={` ${theme?"text-[#fff]":"text-[#7E88C3]"}  text-[15px] font-bold text-right`}>£{item.price}</p>
+            <p className={` ${theme?"text-[#fff]":"text-[#7E88C3]"}  text-[15px] font-bold text-right`}>£{item.total}</p>
           </div>
           <div className='flex justify-between items-center lg:hidden px-5'>
             <div>
@@ -149,7 +147,7 @@ const DetailPage = () => {
         })}
         </div>
 
-        <div className='py-5 flex justify-between items-center px-5 lg:px-10 bg-[#373B53] rounded-b-xl'>
+        <div className={`py-5 flex justify-between items-center px-5 lg:px-10 ${!theme?"bg-[#373B53]" : "bg-[#0C0E16]"} rounded-b-xl`}>
           <p className='text-[#fff] font-medium text-[13px]'>Amount Due</p>
           <p className='text-[#fff] font-bold text-[24px]'>£{totalAmount}</p>
         </div>
